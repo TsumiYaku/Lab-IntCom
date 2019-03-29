2836ec919688c0c0c39417068569bc9cdc39e0b3
TsumiYaku <lorenzo.deluca2407@gmail.com>
2019-03-29 14:56:04 +0100

    Aggiustata funzione SNR per companding

--- LabCom/Lab1v3.m
+++ LabCom/Lab1v3.m
@@ -31,47 +31,74 @@ for i = 1:length(nbit)
     codebook = -V+DV/2:DV:V-DV/2; % Valori quantizzati
     
     % Quantizzazione registrazione vocale
+    [index, quants] = quantiz(sigrec, partition, codebook);
     [lpartition, lcodebook] = lloyds(sigrec, M); % Partizioni algoritmo di Lloyd
-    [lindex, lquants] = quantiz(sigrec,lpartition,lcodebook); % Quantizzazione di Lloyd
+    [lindex, lquants] = quantiz(sigrec, lpartition, lcodebook); % Quantizzazione di Lloyd
     comprec = compand(sigrec, mu, V); % Segnale compresso con compand
-    [cindex, cquants] = quantiz(comprec,partition,codebook); % Quantizzazione con compand
+    [cindex, cquants] = quantiz(comprec, partition, codebook); % Quantizzazione con compand
     
     % Calcolo SNR
+    rSNR = fullSNR(sigrec, index, codebook, peB);
     lrSNR = fullSNR(sigrec, lindex, lcodebook, peB); % SNR con quantizzazione di Lloyd
-    crSNR = fullSNR(comprec, cindex, codebook, peB); % SNR con compand
+    crSNR = zeros(1, length(peB)); % SNR con compand
+    indata = de2bi(cindex);
+    
+    for j = 1:length(peB)
+        outdata = bsc(indata, peB(j));
+        outidx = bi2de(outdata);
+        vout = codebook(outidx+1);
+        vout = compand(vout, mu, V, 'mu/expander');
+        e = sigrec - vout;
+        crSNR(j) = snr(sigrec, e);
+    end
     
     % Quantizzazione file audio
+    [index, quants] = quantiz(sigfile, partition, codebook);
     [lpartition, lcodebook] = lloyds(sigfile, M); % Partizioni algoritmo di Lloyd
-    [lindex, lquants] = quantiz(sigfile,lpartition,lcodebook); % Quantizzazione di Lloyd
+    [lindex, lquants] = quantiz(sigfile, lpartition, lcodebook); % Quantizzazione di Lloyd
     compfile = compand(sigfile, mu, V); % Segnale compresso con compand
-    [cindex, cquants] = quantiz(compfile,partition,codebook); % Quantizzazione con compand
+    [cindex, cquants] = quantiz(compfile, partition, codebook); % Quantizzazione con compand
     
     % Calcolo SNR
+    fSNR = fullSNR(sigfile, index, codebook, peB);
     lfSNR = fullSNR(sigfile, lindex, lcodebook, peB); % SNR con quantizzazione di Lloyd
-    cfSNR = fullSNR(compfile, cindex, codebook, peB); % SNR con compand
+    cfSNR = zeros(1, length(peB)); % SNR con compand
+    indata = de2bi(cindex);
+    
+    for j = 1:length(peB)
+        outdata = bsc(indata, peB(j));
+        outidx = bi2de(outdata);
+        vout = codebook(outidx+1);
+        vout = compand(vout,mu,V, 'mu/expander');
+        e = sigfile - vout;
+        cfSNR(j) = snr(sigfile, e);
+    end
     
     % Plot dei risultati
     figure(1)
-    subplot(3,2,2*(i-1)+1)
+    subplot(3,1,i)
     semilogx(peA,10*log10(SNRt));
     hold on
     grid on
+    semilogx(peB, rSNR, 'o')
     semilogx(peB, lrSNR, 'o')
     semilogx(peB, crSNR, 'o')
     line = ['SNR (voce registrata, ', num2str(nbit(i)), ' bit)'];
     title(line);
-    legend('SNR teorica', 'SNR Lloyd', 'SNR Companding');
+    legend('SNR teorica', 'SNR Uniforme', 'SNR Lloyd', 'SNR Companding');
     xlabel('P_e');
     
-    subplot(3,2,2*(i-1)+2)
+    figure(2)
+    subplot(3,1,i)
     semilogx(peA,10*log10(SNRt));
     hold on
     grid on
+    semilogx(peB, fSNR, 'o')
     semilogx(peB, lfSNR, 'o')
     semilogx(peB, cfSNR, 'o')
     line = ['SNR (file audio, ', num2str(nbit(i)), ' bit)'];
     title(line);
-    legend('SNR teorica', 'SNR Lloyd', 'SNR Companding');
+    legend('SNR teorica', 'SNR Uniforme', 'SNR Lloyd', 'SNR Companding');
     xlabel('P_e');
 end
 
